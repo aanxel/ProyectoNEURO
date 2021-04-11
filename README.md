5867c1a92f1559af1b81217bebc7ef7c8fe8d6ff
Angel Fragua <angelfragua1@gmail.com>
2021-04-11 17:12:57 +0200

    Principio de un gran proyecto

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ProyectoNEURO
+# 
---
+++ try.py
@@ -0,0 +1,11 @@
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %%
+print(np.array([1, 2]))
+
+# %%
+plt.plot([1,2,3], [1,2,3])
+
+# %% [markdown]
